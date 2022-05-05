2c860254cecfd2ac0235fd6eca181418e4a8be82
sun <syoun602@gmail.com>
2022-05-05 23:46:43 +0900

    feat: Bean 설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -27,7 +27,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return  memberRepository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(final Long memberId) {
