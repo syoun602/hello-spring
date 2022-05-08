7626d2fc7e766be8fd72566c8b78636f34d6c702
sun <syoun602@gmail.com>
2022-05-08 17:52:19 +0900

    feat: 스프링 데이터 JPA를 활용한 DB 연결 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,36 +1,37 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+//    private final DataSource dataSource;
+//    private final EntityManager entityManager;
+//
+//    public SpringConfig(final DataSource dataSource, final EntityManager entityManager) {
+//        this.dataSource = dataSource;
+//        this.entityManager = entityManager;
+//    }
 
-    private final DataSource dataSource;
-    private final EntityManager entityManager;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(final DataSource dataSource, final EntityManager entityManager) {
-        this.dataSource = dataSource;
-        this.entityManager = entityManager;
+    public SpringConfig(final MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource); // Memory에서 Jdbc로 변경
 //        return new JdbcTemplateMemberRepository(dataSource); // Jdbc에서 JdbcTemplate으로 변경
-        return new JpaMemberRepository(entityManager);
-    }
+//        return new JpaMemberRepository(entityManager);
+//    }
 }
